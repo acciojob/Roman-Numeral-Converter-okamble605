fc72381e175e672b87e22852c757319dc797e2cf
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-11-14 13:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  // Array of roman numeral mappings in descending order
+  const obj = [
+    ['M', 1000], 
+    ['D', 500], 
+    ['C', 100], 
+    ['L', 50], 
+    ['X', 10], 
+    ['V', 5], 
+    ['I', 1]
+  ];
 
-  //your code here
+  let result = '';  // Result string to hold the roman numeral
 
+  // Loop through each symbol in the array
+  for (let i = 0; i < obj.length; i++) {
+    // While the number is greater than or equal to the current value
+    while (num >= obj[i][1]) {
+      result += obj[i][0];  // Add the symbol to the result
+      num -= obj[i][1];  // Subtract the value from the number
+    }
+  }
+
+  return result;  // Return the final roman numeral
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Testing the function
+console.log(convertToRoman(14));  // Output: XIV
+console.log(convertToRoman(798));  // Output: DCCXCVIII
+console.log(convertToRoman(36));   // Output: XXXVI
